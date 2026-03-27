80c7ecf9210376e05c31eeb5bedc98a2859f5076
Cherwen Molina <cherwen.pictu@gmail.com>
2026-03-27 17:32:42 +0800

    Refactor preliminary_event migration and seeder to include new scoring attributes

--- database/migrations/2024_02_26_072113_create_table_for_preliminary.php
+++ database/migrations/2024_02_26_072113_create_table_for_preliminary.php
@@ -17,17 +17,17 @@ return new class extends Migration
             $table->string('contestant_name');
             $table->string('Address');
 
-            $table->integer('closed_interview')->default(0);
             $table->integer('photogenic')->default(0);
-            $table->integer('white_collection')->default(0);
-            $table->integer('tourism_video')->default(0);
-            $table->integer('talent')->default(0);
-            $table->integer('filipiniana')->default(0);
-            $table->integer('production_wear')->default(0);
             $table->integer('production_number')->default(0);
-            $table->integer('miss_congeniality')->default(0);
-            $table->integer('peoples_choice')->default(0);
+            $table->integer('white_collection')->default(0);
             $table->integer('runway')->default(0);
+            $table->integer('closed_interview')->default(0);
+            $table->integer('talent')->default(0);
+            $table->integer('tourism_video')->default(0);
+            $table->integer('peoples_choice')->default(0);
+            $table->integer('production_wear')->default(0);
+            $table->integer('miss_congeniality')->default(0);
+            $table->integer('filipiniana')->default(0);
 
             $table->integer('total_ranking')->default(0);
             $table->integer('rank')->default(0);
@@ -40,6 +40,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_for_production');
+        Schema::dropIfExists('preliminary_event');
     }
 };
